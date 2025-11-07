4025941a86655f875e73b7f600f2e542a0096597
pakhomovda <pakhomovda@a-r-s.dev>
2025-11-07 07:40:01 +0000

    add setAddress for tcp

--- src/msp/communication/iface/tcpiface.h
+++ src/msp/communication/iface/tcpiface.h
@@ -2,6 +2,7 @@
 #define TCPIFACE_H
 
 #include "absinterface.h"
+#include <cstdint>
 
 using namespace boost::asio;
 class TcpIface : public AbsIface {
@@ -12,6 +13,7 @@ class TcpIface : public AbsIface {
     size_t read(char *buffer, size_t max_length) override;
     size_t available() override;
 
+    void setAddress(std::string ip, uint16_t port);
     void stop() override;
     bool connect() override;
 
@@ -19,6 +21,8 @@ class TcpIface : public AbsIface {
 
   private:
     ip::tcp::socket socket;
+    std::string msp_ip = "127.0.0.1";
+    uint16_t msp_port = 9001;
 
   protected:
     size_t write(char *data, size_t length) override;
